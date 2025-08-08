d6105acee370edf9be4b9c5d0d301892f48d753e
Camilly Santos <camillyvitoriasantosviana@gmail.com>
2025-08-08 11:36:38 +0000

    Término da crição da conexão com o mysql e início dos cruds

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+# app.py
 
 from bd_config import conectar
+from crud import Categoria
 
 def main():
     conexao = conectar
@@ -22,3 +24,26 @@ def main():
 
 if__name__ == "__main__":
     main()
+
+def menu():
+    while True:
+        print("\n=== MENU SGB ===")
+        print("1. Criar Categoria")
+        print("2. Listar Categorias")
+        print("3. Atualizar Categoria")
+        print("4. Deletar Categoria")
+        print("0. Sair")
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            nome = input("Nome da categoria: ")
+            descricao = input("Descrição: ")
+            categoria.criar_categoria(nome, descricao)
+        elif opcao == "2":
+            cats = categoria.listar_categorias()
+            for c in cats:
+                print(f"{c['id']} - {c['nome']} ({c['descricao']})")
+        elif opcao == "3":
+            id_cat = int(Input("ID da categoria: "))
+            nome = input("Novo nome: ")
+            descricao = input("Nova descrição: ")
